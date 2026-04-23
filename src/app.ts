766d6766013662758d9580e362bfeb9bce1676ac
CarlosAlexandredevv <alexfilho062005@gmail.com>
2026-04-23 15:43:29 -0300

    setup zod validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
 import fastify from 'fastify';
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
+import { z } from 'zod';
 
-export const app = fastify();
+export const app = fastify({
+  logger: true,
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.withTypeProvider<ZodTypeProvider>().get(
+  '/health',
+  {
+    schema: {
+      tags: ['health'],
+      response: {
+        200: z.object({
+          status: z.literal('ok'),
+        }),
+      },
+    },
+  },
+  async () => {
+    return { status: 'ok' as const };
+  },
+);
